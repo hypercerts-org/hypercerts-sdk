a0a22fa60da6f24c507434e2982eb8df23871ad3
bitbeckers <code@bitbeckers.com>
2025-01-22 11:37:46 +0100

    feat(coverage): add test coverage checks
    Add test coverage to package.json scripts and github actions. Thresholds
    were set to current state.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,28 @@
-import { defineConfig, defaultExclude } from "vitest/config";
+import { defineConfig, defaultExclude, configDefaults } from "vitest/config";
 
 export default defineConfig({
   test: {
     globalSetup: "./test/setup-env.ts",
     exclude: [...defaultExclude, "./lib/**/*"],
+    coverage: {
+      // you can include other reporters, but 'json-summary' is required, json is recommended
+      reporter: ["text", "json-summary", "json"],
+      // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
+      reportOnFailure: true,
+      thresholds: {
+        lines: 67,
+        branches: 80,
+        functions: 62,
+        statements: 67,
+      },
+      include: ["src/**/*.ts"],
+      exclude: [
+        ...(configDefaults.coverage.exclude as string[]),
+        "**/*.types.ts",
+        "**/types.ts",
+        "src/indexer/*.ts",
+        "lib/*",
+      ],
+    },
   },
 });
